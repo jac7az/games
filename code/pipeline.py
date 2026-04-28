ef529cbdf05e1c0d5c7aaf7cbf27094d761fa393
Carissa <jac7az@gmail.com>
2026-04-28 07:32:22 +0000

    visualizations

--- code/pipeline.py
+++ code/pipeline.py
@@ -1,16 +1,19 @@
 import pandas as pd
 import numpy as np
-from pymongo import MongoClient
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import StandardScaler
 import os
 import logging
+from pymongo import MongoClient
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+import matplotlib.pyplot as plt
+from sklearn.tree import plot_tree
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler("game_processing.log"),
+        logging.FileHandler("pipeline.log"),
         logging.StreamHandler()                   
     ]
 )
@@ -25,27 +28,59 @@ try:
 except Exception as e:
     logging.error("Couldn't connect to mongo")
 
-data = list(game.find())
+logging.info("Creating dataframe")
+data = list(game.find({}))
 df = pd.DataFrame(data)
 
+logging.info("Editing dataframe to match platforms to each game, creating a row per different console for the same games")
 df=df.explode('platforms')
 row=pd.json_normalize(df['platforms'])
 df=pd.concat([df.drop(columns=['platforms','_id']).reset_index(drop=True),row.reset_index(drop=True)],axis=1)
-df=df[df['total_sales']>0]
 
-top_publishers=df['publisher'].value_counts().nlargest(15).index
-df['brands']=df['publisher'].apply(lambda x: x if x in top_publishers else 'Other')
-X=pd.get_dummies(df[['brands']],drop_first=True)
+logging.info("Encoding nonnumeric data")
+publisher_mean=df.groupby('publisher')['total_sales'].mean()
+developer_mean=df.groupby('developer')['total_sales'].mean()
+df['publisher_power']=df['publisher'].map(publisher_mean)
+df['developer_power']=df['developer'].map(developer_mean)
+le=LabelEncoder()
+df['genre_encoded']=le.fit_transform(df['genre'])
+df['console_encoded']=le.fit_transform(df['console'])
+features=['critic_score', 'publisher_power', 'genre_encoded', 'console_encoded', 'release_year']
+X=df[features]
 y=df['total_sales']
-scaler=StandardScaler()
-X_scale=scaler.fit_transform(X)
-model=LinearRegression()
-model.fit(X_scale,y)
-weights=pd.DataFrame({"Feature":X.columns,"Weight":model.coef_}).sort_values(by='Weight')
 
-counts = df['brands'].value_counts().reset_index()
-counts.columns = ['Publisher', 'Count']
-brand_results = weights[weights['Feature'].str.contains('brands')]
-brand_results['Publisher'] = brand_results['Feature'].str.replace('brands', '')
-comparison_df=pd.merge(brand_results,counts,on='Publisher')
-print(comparison_df)
+logging.info("Training Random Forest Regressor Model without developer variable")
+X_train, X_test, y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=756)
+model=RandomForestRegressor(n_estimators=100,random_state=54)
+model.fit(X_train,y_train)
+importances=model.feature_importances_
+features_df=pd.DataFrame({"Features": features, "Weights": importances}).sort_values(by="Weights")
+
+logging.info("Creating feature importance barplot")
+plt.figure(figsize=(10,10))
+plt.barh(features_df['Features'],features_df['Weights'])
+plt.title("Predictors of Video Game Sales (No Developer)")
+plt.xlabel("Weight Influence")
+plt.tight_layout()
+plt.savefig("feature_importance_nodeveloper.png")
+plt.show()
+
+features2=['critic_score', 'publisher_power', 'developer_power','genre_encoded', 'console_encoded', 'release_year']
+X=df[features2]
+y=df['total_sales']
+
+logging.info("Training Random Forest Regressor Model with developer variable")
+X_train, X_test, y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=756)
+model=RandomForestRegressor(n_estimators=100,random_state=54)
+model.fit(X_train,y_train)
+importances=model.feature_importances_
+features_df=pd.DataFrame({"Features": features2, "Weights": importances}).sort_values(by="Weights")
+
+logging.info("Creating feature importance barplot with developer")
+plt.figure(figsize=(10,10))
+plt.barh(features_df['Features'],features_df['Weights'])
+plt.title("Predictors of Video Game Sales")
+plt.xlabel("Weight Influence")
+plt.tight_layout()
+plt.savefig("feature_importance_developer.png")
+plt.show()

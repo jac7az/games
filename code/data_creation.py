021517225d5bd7398bc779a169f07f8289a48d65
Carissa <jac7az@gmail.com>
2026-04-29 00:35:19 -0400

    Update data_creation.py

--- code/data_creation.py
+++ code/data_creation.py
@@ -3,6 +3,8 @@ import json
 import os
 import shutil
 import logging
+
+#Creating log file
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
@@ -14,21 +16,25 @@ logging.basicConfig(
 logging.info("Converting to JSON.")
 
 try:
+    #Load and filter dataset for only 2010-2020 data
     df = pd.read_csv('Video_Games_Sales_Cleaned.csv')
     df = df[df['release_year'] > 2009]
     logging.info(f"Data loaded and filtered successfully")
 except Exception as e:
     logging.error("Failed to load CSV")
 
+#Establish temporary directory for converting individual JSON documents
 output_dir = 'game_documents'
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
+#Grouping by title so one document will contain information for 1 game on all its consoles
 for title, group in df.groupby('title'):
     clean_title = "".join([c for c in str(title) if c.isalnum() or c in (' ', '_')]).strip()
     filename = f"{clean_title}.json"
     filepath = os.path.join(output_dir, filename)
     
+    #Establishing document structure
     document = {
         "title": title,
         "genre": group['genre'].iloc[0],
@@ -37,6 +43,7 @@ for title, group in df.groupby('title'):
         "platforms": []
     }
     
+    #Add specific sales and score data per console
     for _, row in group.iterrows():
         platform_info = {
             "console": row['console'],
@@ -46,11 +53,13 @@ for title, group in df.groupby('title'):
         }
         document["platforms"].append(platform_info)
     
+    #Complete the JSON file
     with open(filepath, 'w') as f:
         json.dump(document, f, indent=4)
 
 logging.info(f"Created {len(df.groupby('title'))} platform-accurate JSON documents.")
 
+#Create output file into a single zip file with the option to autodownload it.
 try:
     output_filename = 'game_documents_combined_archive'
     shutil.make_archive(output_filename, 'zip', output_dir)
